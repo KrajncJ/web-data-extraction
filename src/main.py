b59ecc9bdbab4471d05ed6a8b656447dbd4d53fa
Jaka Krajnc <jkrajnc11@gmail.com>
2019-04-20 11:12:51 +0200

    rtvslo regex parsing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,27 @@
 
+import regex
+import os
+from selenium import webdriver
+
+
+# Not used currently, may use if content will be displayed with JS. Then we have to render page with browser
+def open_page(path):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    driver.get("file:///" + os.path.abspath(path))
+
+    return driver.page_source
+
 
 if __name__ == '__main__':
-    print("Initial commit")
+    # Get HTML content
+    overstock_01 = open('../data/overstock.com/jewelry01.html').read()
+    overstock_02 = open('../data/overstock.com/jewelry01.html').read()
+    rtvslo_01 = open('../data/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html').read()
+    rtvslo_02 = open('../data/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html').read()
 
+    parsed_rtvslo01 = regex.parse_rtvslo(rtvslo_01)
+    parsed_rtvslo02 = regex.parse_rtvslo(rtvslo_02)
+    print(parsed_rtvslo01)
+    print(parsed_rtvslo02)
--- src/regex.py
+++ src/regex.py
@@ -1,1 +1,39 @@
 
+import json
+import re
+
+
+def parse_overstock(html):
+    print('not implemented')
+
+
+def parse_amazon(html):
+    print('not implemented')
+
+
+def parse_rtvslo(html):
+    # Author Name
+    author = re.findall('<div class="author-name">(.+?)</div>', html)[0]
+    # Publis Time - Issues with dots, used more expressions
+    pub_time = re.findall('<div class="publish-meta">\n\t\t(.+?). (.+?) ob (.+?)<br>', html)
+    pub_time = pub_time[0][0] + ". " + pub_time[0][1] + " ob " + pub_time[0][2]
+    # Title
+    title = re.findall('<h1>(.+?)</h1>', html)[0]
+    # Subtitle
+    subtitle = re.findall('<div class="subtitle">(.+?)</div>', html)[0]
+    # Lead
+    lead = re.findall('<p class="lead">(.+?)</p>', html)[0]
+
+    # Content @TODO . does not include all chars, following regex does not work.
+    # content = re.findall('<article class="article">(.+?)</article>', html)[0]
+    content = 'Not implemented yet'
+
+    return json.dumps({
+        "Author": author,
+        "PublishedTime": pub_time,
+        "Title": title,
+        "SubTitle": subtitle,
+        "Lead": lead,
+        "Content": content,
+    }, indent=2)
+
